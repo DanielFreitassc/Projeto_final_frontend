80c7fcbaf36d5817ba14fd4dcaefe9fe94f8317d
DanielFreitassc <danielfreitassc@gmail.com>
2024-06-11 20:34:03 -0300

    Feat: Fazendo os estilos do Footer

--- src/components/Footer/styles.tsx
+++ src/components/Footer/styles.tsx
@@ -5,15 +5,18 @@ export const FooterContainer = styled.div`
     background-color: #171717;
     height: 130px;
     width: 100%;
+    padding: 8px;
 `
 export const CopyContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: flex-end;
     height: 100%;
-    padding: 24px;
+    margin-right: 32px;
 `
 
 export const Copy = styled.p`
     color: white;
+    padding: 16px;
+    margin-top: 40px;
 `
